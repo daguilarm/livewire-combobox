6b6a5106fc4f342ca1d81ccd9a33e7eda627998f
daguilarm <damian.aguilarm@gmail.com>
2021-05-01 14:56:55 +0200

    testing livewire with dusk

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -4,26 +4,34 @@ declare(strict_types=1);
 
 namespace Daguilarm\LivewireCombobox\Tests;
 
+use Closure;
 use Daguilarm\LivewireCombobox\LivewireComboboxServiceProvider;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
-use Laravel\Dusk\Browser;
 use Livewire\LivewireServiceProvider;
 use Livewire\Macros\DuskBrowserMacros;
+use Orchestra\Testbench\Dusk\Foundation\Console\DuskCommand;
+use Orchestra\Testbench\Dusk\Options as DuskOptions;
 use Orchestra\Testbench\Dusk\TestCase;
+use Psy\Shell;
 
 class BrowserTestCase extends TestCase
 {
+    use SupportsSafari;
+
+    public static $useSafari = false;
+
     public function setUp(): void
     {
         // DuskOptions::withoutUI();
         if (isset($_SERVER['CI'])) {
             DuskOptions::withoutUI();
         }
-
-        Browser::mixin(new DuskBrowserMacros);
 
         // Clean up the test
         $this->afterApplicationCreated(function () {
@@ -36,6 +44,7 @@ class BrowserTestCase extends TestCase
 
         parent::setUp();
 
+        // Define the testing routes
         $this->tweakApplication(function () {
             //Routes for testing
             Route::get('/testing', function() {
@@ -65,15 +74,7 @@ class BrowserTestCase extends TestCase
             __DIR__.'/../tests/Browser/resources/views',
             resource_path('views'),
         ]);
-        $app['config']->set('auth.providers.users.model', User::class);
-        $app['config']->set('session.driver', 'file');
         $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
     }
 
     /**
@@ -97,6 +98,63 @@ class BrowserTestCase extends TestCase
         preg_match_all('/dusk="(.*)"/', $html, $results);
 
         return $results[1];
+    }
+
+    protected function driver(): RemoteWebDriver
+    {
+        $options = DuskOptions::getChromeOptions();
+
+        $options->setExperimentalOption('prefs', [
+            'download.default_directory' => __DIR__.'/downloads',
+        ]);
+
+        return static::$useSafari
+            ? RemoteWebDriver::create(
+                'http://localhost:9515', DesiredCapabilities::safari()
+            )
+            : RemoteWebDriver::create(
+                'http://localhost:9515',
+                DesiredCapabilities::chrome()->setCapability(
+                    ChromeOptions::CAPABILITY,
+                    $options
+                )
+            );
+    }
+
+    public function browse(Closure $callback)
+    {
+        parent::browse(function (...$browsers) use ($callback) {
+            try {
+                $callback(...$browsers);
+            } catch (Exception $e) {
+                if (DuskOptions::hasUI()) $this->breakIntoATinkerShell($browsers, $e);
+
+                throw $e;
+            } catch (Throwable $e) {
+                if (DuskOptions::hasUI()) $this->breakIntoATinkerShell($browsers, $e);
+
+                throw $e;
+            }
+        });
+    }
+
+    public function breakIntoATinkerShell($browsers, $e)
+    {
+        $sh = new Shell();
+
+        $sh->add(new DuskCommand($this, $e));
+
+        $sh->setScopeVariables([
+            'browsers' => $browsers,
+        ]);
+
+        $sh->addInput('dusk');
+
+        $sh->setBoundObject($this);
+
+        $sh->run();
+
+        return $sh->getScopeVariables(false);
     }
 
     /**
